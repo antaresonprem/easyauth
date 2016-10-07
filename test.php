2116328658fd59345ca26d5447d3d21a688b22b2
Chun Ye <chunye@microsoft.com>
2016-10-06 23:18:10 -0700

    EasyAuth demo deployment.

---
+++ test.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>test</h1>
+    <div>Logged in as
+        <?php
+            echo $_SERVER['HTTP_X_MS_CLIENT_PRINCIPAL_NAME']
+         ?>
+    </div>
+    <form action="/test.php" method="POST">
+        <input class="form-control" id="hoge" name="hoge" type="text" value=""/>
+        <input type="submit" value="Test PHP" class="btn"/>
+    </form>
+<h1>result</h1>
+<?php
+	echo $_POST['hoge']
+?>
+</body>
+</html>
